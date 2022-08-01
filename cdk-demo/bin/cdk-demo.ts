619cf3b7ad52d6e74d3f21ed69caee318af04b11
Minjie Xu <minjiexu@mini-de-minjie.lan>
2022-08-01 16:43:36 -0500

    fix cdk-demo project

--- cdk-demo/bin/cdk-demo.ts
+++ cdk-demo/bin/cdk-demo.ts
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
+//import * as cdk from 'aws-cdk-lib';
+import * as cdk from '@aws-cdk/core';
 import { CdkDemoStack } from '../lib/cdk-demo-stack';
 
-//const app = new cdk.App();
-//new CdkDemoStack(app, 'CdkDemoStack', {
+const app = new cdk.App();
+new CdkDemoStack(app, 'CdkDemoStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -15,40 +16,8 @@ import { CdkDemoStack } from '../lib/cdk-demo-stack';
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  //env: { account: 'my-aws-acct-id', region: 'us-east-1' },
+  env: { account: 'use aws sts get-caller-identity to get acct id', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-//});
-// 
-// https://aws.amazon.com/getting-started/guides/setup-cdk/module-three/
-// 
-// I have not tried cdk deploy yet, as npm run build gave 6 errors
-// 
-export class CdkDemoStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+});
 
-    // The code that defines your stack goes here
-    // CHANGE: We have created the vpc object from the Vpc class.
-    const vpc = new Vpc(this, 'MainVpc',{
-    
-    // CHANGE: this is where we define how many AZs to use
-    maxAzs: 2,
-      
-   // CHANGE: We define a single subnet configuration per AZ.
-      subnetConfiguration:  [
-        {
-          // CHANGE: this is it's CIDR mask so 255.255.255.0
-          cidrMask: 24,
-
-          // CHANGE: a name for each of these subnets
-          name: 'public-subnet',
-
-          // CHANGE: and the subnet type to be used - here we will have
-          // a public subnet. There are other options available here.
-          subnetType: SubnetType.PUBLIC
-        },
-      ]
-    });
-  }
-}
--- cdk-demo/lib/cdk-demo-stack.ts
+++ cdk-demo/lib/cdk-demo-stack.ts
@@ -1,16 +1,37 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+//import { Stack, StackProps } from 'aws-cdk-lib';
+//import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from '@aws-cdk/core';
+import { Vpc, SubnetType } from '@aws-cdk/aws-ec2';
 
-export class CdkDemoStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+//
+// https://aws.amazon.com/getting-started/guides/setup-cdk/module-three/
+// 
+export class CdkDemoStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    // CHANGE: We have created the vpc object from the Vpc class.
+    const vpc = new Vpc(this, 'MainVpc',{
+    
+    // CHANGE: this is where we define how many AZs to use
+    maxAzs: 2,
+      
+   // CHANGE: We define a single subnet configuration per AZ.
+      subnetConfiguration:  [
+        {
+          // CHANGE: this is it's CIDR mask so 255.255.255.0
+          cidrMask: 24,
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkDemoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+          // CHANGE: a name for each of these subnets
+          name: 'public-subnet',
+
+          // CHANGE: and the subnet type to be used - here we will have
+          // a public subnet. There are other options available here.
+          subnetType: SubnetType.PUBLIC
+        },
+      ]
+    });
   }
 }

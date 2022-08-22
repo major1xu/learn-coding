65c4d27dfa8bde0d29acedfe4ef4baf245b4fa93
Minjie Xu <minjiexu@minjies-mbp.lan>
2022-08-22 11:37:27 -0500

    add code for asyn and restful end point, work in progress

--- authentication-vanilla-js/index.js
+++ authentication-vanilla-js/index.js
@@ -1,8 +1,42 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
-
+app.use(bodyParser.json())
 app.use(express.static('public'))
+
+// store in memory
+let meals = []
+getUser((err, user) => {
+  if (err) {
+    return console.log(err)
+  }
+  getPostsByUserId(user.id, (err, posts) => {
+    if (err) {
+      return console.log(err)
+    }
+    console.log('found %s posts', posts.length)
+  })
+)
+app.get('/meals', (req, res) => {
+  return res.send(meals)
+})
+
+app.post('/meals', (req, res) => {
+  let meal = req.body
+  meal.id = Date.now()
+  meals.push(meal)
+  return res.send(meal)
+})
+
+app.delete('/meals/:id', (req, res) => {
+  let id = parseInt(req.params.id)
+  const index = meals.map(o => o.id).indexOf(id)
+  if (index !== -1) {
+    meals.splice(index, 1)
+  }
+  return res.send()
+})
 
 app.listen(8080, () => {
   console.log('App listening on port 8080!')
--- authentication-vanilla-js/public/main.js
+++ authentication-vanilla-js/public/main.js
@@ -18,6 +18,25 @@ class App {
     ]
     this.render()
   }
+  request (method, url, data = null) {
+  return new Promise((resolve, reject) => {
+    let xhr = new XMLHttpRequest()
+    xhr.open(method, url, true)
+    xhr.setRequestHeader('Content-Type', 'application/json')
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        return resolve(JSON.parse(xhr.responseText || '{}'))
+      } else {
+        return reject(new Error(`Request failed with status ${xhr.status}`))
+      }
+    }
+    if (data) {
+      xhr.send(JSON.stringify(data))
+    } else {
+      xhr.send()
+    }
+  })
+}
   addMeal (meal) {
     document.getElementById('meals').appendChild(this.createMealElement(meal))
     this.meals.push(meal)

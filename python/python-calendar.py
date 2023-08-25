3ed11373139ef2802feca77feed54cf8a03a4dd0
major1xu <minjie.xu@gmail.com>
2023-08-24 22:00:22 -0500

    add more comments for the python code: came across them from Twitter

--- python/python-calendar.py
+++ python/python-calendar.py
@@ -1,7 +1,13 @@
-from calendar import*
+# Original
+# https://twitter.com/clcoding/status/1694534239235260859
+# Modified
+# https://twitter.com/stlplace/status/1694902410584420516
+
+from calendar import calendar
 year = int(input('Enter Year:'))
 print(calendar(year,2,1,8.3))
 
+# https://twitter.com/clcoding/status/1694560164370604172
 #Ukraine Flag using Python
 
 import numpy as np

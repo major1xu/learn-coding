adcbb3411a5e45ee7efbdd67aa9533e6228870d6
major1xu <minjie.xu@gmail.com>
2023-08-29 10:10:59 -0500

    add sopme more python exercise

---
+++ python/change-list-items.py
@@ -0,0 +1,14 @@
+# https://www.w3schools.com/python/python_lists_change.asp 
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+print(thislist)
+
+# Change the values "banana" and "cherry" with the values "blackcurrant" and "watermelon":
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[1:3] = ["blackcurrant", "watermelon"]
+print(thislist)
+
+# Change the second value by replacing it with two new values:
+thislist = ["apple", "banana", "cherry"]
+thislist[1:2] = ["blackcurrant", "watermelon"]
+print(thislist)
---
+++ python/list-pop-sum.py
@@ -0,0 +1,7 @@
+# https://twitter.com/RealBenjizo/status/1696230485725163523 
+my_list=["Hello", 1, 3, 4, 7, "World"]
+
+num1 = my_list.pop(2)
+num2 = my_list.pop(3)
+
+print(sum((num1,num2)))

b346285a9fb4b7c9c77d620b51d4d11d9fabf1ac
major1xu <minjie.xu@gmail.com>
2025-12-12 18:41:31 -0600

    add comments for the python sample code

--- python/SpeakTextAsAudio.py
+++ python/SpeakTextAsAudio.py
@@ -1,5 +1,6 @@
 import pyttsx3
-
+# https://x.com/clcoding/status/1991003892428255539 
+# source code: clcoding.com
 engine = pyttsx3.init()
 msg = input("Enter text to speak: ")
 speed = int(input("speed (100-300): "))

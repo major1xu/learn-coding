dfa4d29a0d749755de524e876c440103658022db
major1xu <minjie.xu@gmail.com>
2026-04-07 17:14:33 -0500

    accept a dynamic URL

--- python/pandas-read-html.py
+++ python/pandas-read-html.py
@@ -1,4 +1,14 @@
 import pandas as pd
+import argparse
+
+parser = argparse.ArgumentParser(description="A sample script")
+parser.add_argument("name", help="Your name")  # Positional argument
+parser.add_argument("-v", "--verbose", action="store_true", help="Increase output verbosity")  # Optional flag
+
+args = parser.parse_args()
+print(f"Hello, {args.name}")
+if args.verbose:
+    print("Verbosity is on")
 
 # Fetch all tables from a webpage
 url = "https://docs.google.com/document/d/e/2PACX-1vTMOmshQe8YvaRXi6gEPKKlsC6UpFJSMAk4mQjLm_u1gmHdVVTaeh7nBNFBRlui0sTZ-snGwZM4DBCT/pub"
@@ -6,11 +16,9 @@ tables = pd.read_html(url, encoding='utf-8')
 
 # Select the first table on the page
 df = tables[0]
-print(df.head())
-#print(df.info())
 
-# Create a 5x5 grid filled with spaces
-grid = [[" " for _ in range(4)] for _ in range(4)]
+grid_size = len(df)
+grid = [[" " for _ in range(grid_size)] for _ in range(grid_size)]
 
 for row in df.itertuples():
     print(row[1], row[3], row[2])  # index 0 is the row index, index 1 is the first column

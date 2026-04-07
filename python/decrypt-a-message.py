32cb7171dffb83b7e39886d05707e8fe722fc742
major1xu <minjie.xu@gmail.com>
2026-04-07 18:15:50 -0500

    command line argument is working; need to check the output, it's possible that the y axis is messed up because of the loop

--- python/decrypt-a-message.py
+++ python/decrypt-a-message.py
@@ -1,8 +1,14 @@
-import pandas as pd
+# to run it on a command line window, do this
+# python decrypt-a-message.py "https://docs.google.com/document/d/e/2PACX-1vTMOmshQe8YvaRXi6gEPKKlsC6UpFJSMAk4mQjLm_u1gmHdVVTaeh7nBNFBRlui0sTZ-snGwZM4DBCT/pub"
+# tested using python 3.11.3 via 
+# lias python='~/anaconda3/bin/python'
+# 
 import argparse
+import pandas as pd
 
 parser = argparse.ArgumentParser(description="A sample script")
-parser.add_argument("fileURL", help="Input file URL")  # Positional argument
+parser.add_argument("fileURL", help="Input file URL", type=str)  # Positional argument
+#parser.add_argument("square", help="display a square of a given number", type=int)
 parser.add_argument("-v", "--verbose", action="store_true", help="Increase output verbosity")  # Optional flag
 
 args = parser.parse_args()
@@ -10,10 +16,12 @@ print(f"Hello, {args.fileURL}")
 if args.verbose:
     print("Verbosity is on")
 
+#print(args.square**2)
+
 # Fetch all tables from a webpage
-url = {args.fileURL}
+url = args.fileURL
 print(url)
-tables = pd.read_html(url, encoding='utf-8')
+tables = pd.read_html(url, encoding="utf-8")
 
 # Select the first table on the page
 df = tables[0]
